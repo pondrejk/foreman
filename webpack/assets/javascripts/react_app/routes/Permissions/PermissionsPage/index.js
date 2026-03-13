488330aff41a51c58710184d47cd85a00b87bc99
Peter Ondrejka <pondrejk@redhat.com>
2026-03-13 10:33:03 -0400

    dummy table

--- webpack/assets/javascripts/react_app/routes/Permissions/PermissionsPage/index.js
+++ webpack/assets/javascripts/react_app/routes/Permissions/PermissionsPage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { TextContent, Text, TextVariants } from '@patternfly/react-core';
+import { Table, Thead, Tr, Th, Tbody, Td, TableText, Caption } from '@patternfly/react-table';
 
 import PageLayout from '../../common/PageLayout/PageLayout';
 import { translate as __ } from '../../../common/I18n';
@@ -10,14 +11,28 @@ const PermissionsPage = () => (
   <PageLayout header={__('Permissions')} searchable={false}>
     <>
       <TextContent>
-        <Text ouiaId="upgrade-page-top-msg" component={TextVariants.p}>
+        <Text ouiaId="" component={TextVariants.p}>
           {__(
             'Stay secure, supported, and up-to-date by upgrading to the latest version of Foreman.' +
               ' Access new features, critical updates, and enhanced compatibility with minimal disruption.'
           )}
         </Text>
       </TextContent>
-      <br />
+      <Table aria-label="Simple table" variant='compact' borders='compactBorderless'>
+        <Caption>Resource type</Caption>
+        <Thead>
+          <Tr>
+            <Th>{__('Permission Name')}</Th>
+            <Th>{__('Actions')}</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td><TableText>{'Name'}</TableText></Td>
+            <Td><TableText>{'Description'}</TableText></Td>
+          </Tr>
+        </Tbody>
+      </Table>
     </>
   </PageLayout>
 );
